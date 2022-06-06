8ee23e7ac46bb24f5987185fae13656f8e4aa494
Rati.Shoshikelashvili6911@ens.tsu.edu.ge <Rati.Shoshikelashvili6911@ens.tsu.edu.ge>
2022-06-06 08:12:25 +0400

    menu-top-responsive

--- functions.php
+++ functions.php
@@ -11,9 +11,19 @@ function resorttheme_menus(){
 
 function resorttheme_register_styles(){
     wp_enqueue_style('resorttheme-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('resorttheme-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css");
+    wp_enqueue_style('resorttheme-font', "db.onlinewebfonts.com/c/924ee4158324c02e3e0d62e54cafd14f?family=Gotham+Book");
 }
 
 add_action('init', 'resorttheme_menus');
 add_action('wp_enqueue_scripts', 'resorttheme_register_styles');
+
+function resorttheme_register_scripts(){
+    wp_enqueue_script('resorttheme-jquery', "https://code.jquery.com/jquery-3.2.1.slim.min.js", array(), false, true);
+    wp_enqueue_script('resorttheme-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js", array(), false, true);
+    wp_enqueue_script('resorttheme-bootstrapjs', "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js", array(), false, true);
+}
+
+add_action('wp_enqueue_scripts', 'resorttheme_register_scripts');
 
 ?>
--- header.php
+++ header.php
@@ -10,6 +10,25 @@
             <div id="logo">
                 <img src="<?php echo get_template_directory_uri();; ?>\assets\images\logo.png"></img>
             </div>
-            <div id="menus"></div>
+            <div id="menus">
+
+                <nav class="navbar navbar-expand-lg navbar-light"> 
+                    <div class="container"> 
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav"> 
+                            <?php
+                            wp_nav_menu(
+                                array(
+                                    'theme_location' => 'top',
+                                    'menu_class'=>'navbar-nav'
+                                )
+                                );
+                            ?>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
     </header>
