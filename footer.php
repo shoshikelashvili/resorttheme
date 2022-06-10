a87c5dd9ec0a3631932c1f3c1d3b2b17173d2a54
Rati.Shoshikelashvili6911@ens.tsu.edu.ge <Rati.Shoshikelashvili6911@ens.tsu.edu.ge>
2022-06-10 17:05:55 +0400

    finish inital version of markup

--- footer.php
+++ footer.php
@@ -1,5 +1,26 @@
 <div id="footer">
-    <h1>Footer</h1>
+    <div id="bottom-navigation">
+        <div id="bottom-logo">
+            <img src="<?php echo get_template_directory_uri();; ?>\assets\images\logo.png"></img>
+        </div>
+        <div id="menus">
+            <nav class="navbar navbar-expand-lg navbar-light"> 
+                <div class="container"> 
+                        <?php
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'bottom',
+                                'menu_class'=>'navbar-nav'
+                            )
+                            );
+                        ?>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <div id="copyright">
+        @ COPYRIGHT <?php echo date("Y");?>
+    </div>
 </div>
 <?php wp_footer()?>
 </body>
